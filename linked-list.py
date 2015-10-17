e8d07a8feec11db7d589a389dcbeda967f7cf142
guand <guan3d@gmail.com>
2015-10-17 04:24:57 -0700

    Added singly linked-list

--- linked-list.py
+++ linked-list.py
@@ -1,15 +1,13 @@
-from node import Node
+from node import SingleList
 
 def main():
-	node1 = Node(1)
-	node2 = Node(2)
-	node3 = Node(3)
-	node4 = Node(6)
-	node1.next = node2
-	node2.next = node3
-	node3.next = node4
-
-	print_list(node1)
+	l = SingleList()
+	l.append(1)
+	l.append(3)
+	l.append(9)
+	l.remove(3)
+	l.remove(55)
+	l.show()
 
 def print_list(node):
 	while node:
--- node.py
+++ node.py
@@ -6,4 +6,38 @@ class Node:
 	def __str__(self):
 		return str(self.data)
 
+
+class SingleList:
+	head = None
+	tail = None
+
+	def append(self, data):
+		node = Node(data)
+		if self.head is None:
+			self.head = self.tail = node
+		else:
+			self.tail.next = node
+		self.tail = node
+
+	def show(self):
+		current_node = self.head
+		while current_node is not None:
+			print current_node.data, "-> ",
+			current_node = current_node.next
+		print None
+
+	def remove(self, data):
+		current_node = self.head
+		previous_node = None
+		while current_node is not None:
+			if current_node.data == data:
+				if previous_node is not None:
+					previous_node.next = current_node.next
+				else:
+					self.head = current_node
+
+			previous_node = current_node
+			current_node = current_node.next
+
+
 	
